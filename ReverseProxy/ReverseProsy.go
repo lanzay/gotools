d113a5c0376aa30c59ce1470abb962be5b9b1a67
lanzay <lanzay@gmail.com>
2018-03-10 01:00:41 +0300

    ReverseProxy

---
+++ ReverseProxy/ReverseProsy.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"net/http/httputil"
+	"net/url"
+	"net/http"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"strconv"
+)
+
+type (
+	Proxy struct {
+		ListenPort  int
+		DefaultHost string
+		Hosts       []Host
+		Proxy       map[string]*httputil.ReverseProxy
+	}
+	Host struct {
+		Host   string
+		Target string
+	}
+)
+
+var proxyCfg Proxy
+
+func main() {
+	
+	proxyCfg.Proxy = make(map[string]*httputil.ReverseProxy)
+	if _, err := toml.DecodeFile("proxy.cfg", &proxyCfg); err != nil {
+		fmt.Println(err)
+	}
+	
+	for _, v := range proxyCfg.Hosts {
+		target, _ := url.Parse(v.Target)
+		proxyCfg.Proxy[v.Host] = httputil.NewSingleHostReverseProxy(target)
+	}
+	
+	http.HandleFunc("/", handleProxy)
+	http.ListenAndServe(":"+strconv.Itoa(proxyCfg.ListenPort), nil)
+}
+
+func handleProxy(w http.ResponseWriter, r *http.Request) {
+	
+	dr, _ := httputil.DumpRequest(r, false)
+	fmt.Println(string(dr))
+	
+	if rp, ok := proxyCfg.Proxy[r.Host]; ok == true {
+		rp.ServeHTTP(w, r)
+	} else {
+		fmt.Printf("[ERR] Default Host %s\n", proxyCfg.DefaultHost)
+		proxyCfg.Proxy[proxyCfg.DefaultHost].ServeHTTP(w, r)
+	}
+	
+}
